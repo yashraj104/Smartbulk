328e48c554819eb508675e80d2b1facc17c92c38
Yashraj <yashrajshinde0225@gmail.com>
2025-08-31 19:23:29 +0530

    security reasons

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -6,13 +6,13 @@ import { getAnalytics } from 'firebase/analytics';
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyBWQ-wULKR2cFIQYoC_RXZdsrzj_c0Q6-Y",
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN || "smartbulk-efd24.firebaseapp.com",
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "smartbulk-efd24",
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET || "smartbulk-efd24.firebasestorage.app",
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID || "83687074580",
-  appId: process.env.REACT_APP_FIREBASE_APP_ID || "1:83687074580:web:5776b9eb0f429a4469b001",
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID || "G-CSQJD5XH3P"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY ,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN ,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID ,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID ,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID ,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID 
 };
 
 // Initialize Firebase
